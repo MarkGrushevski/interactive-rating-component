988b6d2d6efa890a3b163793feefdfd649317045
MarkGrushevski <markgrushevski@gmail.com>
2022-12-11 13:45:29 +0500

    temp

---
+++ src/components/RatingCard.jsx
@@ -0,0 +1,15 @@
+import "./RatingCard.css";
+
+export const RatingCard = () => {
+  return (
+    <article className="rating-card">
+      <div className="rating-card">
+        <h2 className="rating-card__title">How did we do?</h2>
+        <p className="rating-card__description">
+          Please let us know how we did with your support request. All feedback
+          is appreciated to help us improve our offering!
+        </p>
+      </div>
+    </article>
+  );
+};
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RatingCard } from "./components/RatingCard.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RatingCard />
+  </React.StrictMode>
+);
